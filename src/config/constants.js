8d99a0520d7f00f84bd53b49276810e8860e0b16
Ron Dyck <rond@webbtech.net>
2018-07-27 11:32:21 -0400

    removed config files

--- src/config/constants.js
+++
@@ -1,5 +0,0 @@
-
-export const RECORDS_START_YEAR = 2013
-export const STD_DATE_FORMAT = 'YYYY-MM-DD'
-export const LOGROCKET_ID = 'cee2gx/gales-dips2'
-export const FUEL_TYPE_LIST = ['NL', 'SNL', 'DSL', 'CDSL']
