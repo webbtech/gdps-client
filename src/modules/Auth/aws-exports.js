4882c3d424bb0cf6a0e273617037b215b1b26084
Ron Dyck <rond@webbtech.net>
2020-01-27 10:25:39 -0500

    check cognito pool

--- src/modules/Auth/aws-exports.js
+++ src/modules/Auth/aws-exports.js
@@ -1,11 +1,17 @@
 
-const aws_exports = {
+/* const aws_exports = {
   Auth: {
     region: 'us-east-1',
     userPoolId: 'us-east-1_gsB59wfzW',
     userPoolWebClientId: '2084ukslsc831pt202t2dudt7c',
   },
+} */
+const awsExports = {
+  Auth: {
+    region: 'ca-central-1',
+    userPoolId: 'ca-central-1_lolwfYIAr',
+    userPoolWebClientId: '5n63nd473pv7ne2qskv30gkcbh',
+  },
 }
 
-export default aws_exports
-
+export default awsExports
